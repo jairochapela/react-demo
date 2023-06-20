4931db9621d06d9202da11bb38d68f0435a943e7
Jairo Chapela-Martinez <jairochapela@gmail.com>
2023-06-20 09:40:19 +0000

    Eventos

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import './App.css';
+import { useState } from 'react';
 
-var tasks = ["Alpha", "Bravo", "Charlie"];
+
+
+
 
 function Task(props) {
   let name = props.name;
+  let onUpdate = props.onUpdate;
+
   return (
-    <li><input type="checkbox" />{name}</li>
+    <li id={name}><input type="checkbox" onChange={() => onUpdate(name)} />{name}</li>
   )
 }
 
 function TaskList(props) {
   let tasks = props.tasks;
+  let onUpdate = props.onUpdate;
   return (
     <ul type="none">
-      {tasks.map(t => (<Task name={t}></Task>))}
+      {tasks.map((t,i) => (<Task key={"task-"+i} name={t} onUpdate={onUpdate} />))}
     </ul>
   )  
 }
 
 function App() {
+
+  var [tasks, setTasks] = useState(["Alpha", "Bravo", "Charlie"]);
+
+  function removeTask(name) {
+    console.log("Removing task: " + name);
+    setTasks(tasks.filter(t => t !== name));
+  }
+  
   return (
     <div className="App">
-      <TaskList tasks={tasks}/>
+      <TaskList tasks={tasks} onUpdate={removeTask} />
     </div>
   );
 }
